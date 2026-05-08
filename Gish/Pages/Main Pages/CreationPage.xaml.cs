2b98288ba1e4ecf66b79b7283ed6e88f1e1e7156
Bryson <rikbrysn@gmail.com>
2026-05-08 09:23:48 +0800

    Small Additions [05/08/2026]
    - Added the bugfixes from before in the Creation Page and Rules Page for consistency.

--- Gish/Pages/Main Pages/CreationPage.xaml.cs
+++ Gish/Pages/Main Pages/CreationPage.xaml.cs
@@ -2,13 +2,47 @@ namespace Gish.Pages.MainPages;
 
 public partial class CreationsPage : ContentPage
 {
+    private List<Button> cachedButtons = new List<Button>();
+    private List<ImageButton> cachedImgButtons = new List<ImageButton>();
+    
     public CreationsPage()
     {
         InitializeComponent();
     }
+    
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        
+        setAllButtonState(true);
+    }
+    
+    protected override void OnHandlerChanged()
+    {
+        base.OnHandlerChanged();
+
+        cachedButtons = App.getAllButtons(this);
+        cachedImgButtons = App.getAllImageButtons(this);
+
+        setAllButtonState(true);
+    }
 
     private async void goToProfilePage(object? sender, EventArgs e)
     {
-        await Shell.Current.GoToAsync("//ProfilePage");
+        try
+        {
+            setAllButtonState(false);
+            await Shell.Current.GoToAsync("//ProfilePage");
+        }
+        catch (Exception ex)
+        {
+            setAllButtonState(true);
+        }
+    }
+    
+    private void setAllButtonState(bool enable)
+    {
+        App.setButtonState(cachedButtons, enable);
+        App.setImageButtonState(cachedImgButtons, enable);
     }
 }
