0aa2b493a06707c2e1bfc4f63ca46a6643af5825
Bryson <rikbrysn@gmail.com>
2026-05-23 18:24:42 +0800

    Updated Tag Images

---
+++ Gish/Pages/Main Pages/Tools Pages/Initiative Tracker.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gish.Pages.Main_Pages.Tools_Pages;
+
+public partial class Initiative_Tracker : ContentPage
+{
+    public Initiative_Tracker()
+    {
+        InitializeComponent();
+    }
+}
